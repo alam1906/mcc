79641436d107b77737d59489d2a9d83760bb3f19
alam1906 <107329748+alam1906@users.noreply.github.com>
2025-12-13 18:12:39 -0800

    create navbar

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import { Menu, X } from '@lucide/svelte';
+	import { menus } from '$lib/data/menus';
+	import { slide } from 'svelte/transition';
+	import { MediaQuery } from 'svelte/reactivity';
+
+	const desktop = $derived(new MediaQuery('max-width: 640px').current);
+
+	let isOpen = $state(true);
+</script>
+
+<div class={`sticky top-0 w-full bg-white ${isOpen ? '' : 'shadow-md'}`}>
+	<div class="mx-auto max-w-6xl">
+		<div class="mx-5">
+			<div class="flex h-20 items-center justify-between">
+				<!-- logo -->
+				<div class="font-serif text-2xl font-semibold">MCC</div>
+
+				<!-- navbar desktop -->
+				<div class="hidden items-center gap-x-10 sm:flex">
+					{#each menus as item}
+						<a
+							href={item.path}
+							class={`${page.url.pathname === item.path ? 'text-blue-600' : ''}  font-semibold`}
+							>{item.name}</a
+						>
+					{/each}
+				</div>
+
+				<!-- trigger button -->
+				<button
+					onclick={() => {
+						isOpen = !isOpen;
+					}}
+					class="cursor-pointer sm:hidden"
+				>
+					{#if isOpen && desktop}
+						<X />
+					{:else}
+						<Menu />
+					{/if}
+				</button>
+			</div>
+		</div>
+		<!-- mobile -->
+	</div>
+	{#if isOpen}
+		<div
+			transition:slide={{ duration: 300 }}
+			class="absolute top-20 w-full bg-white shadow-md sm:hidden"
+		>
+			<div class="flex w-full flex-col items-center justify-center">
+				{#each menus as item}
+					<a
+						href={item.path}
+						class={`w-full py-5 text-center font-semibold ${page.url.pathname === item.path ? 'text-blue-600' : ''}`}
+						>{item.name}</a
+					>
+				{/each}
+			</div>
+		</div>
+	{/if}
+</div>
---
+++ src/lib/data/menus.ts
@@ -0,0 +1,6 @@
+export const menus = [
+	{ id: 1, name: 'Beranda', path: '/' },
+	{ id: 2, name: 'Anggota', path: '/anggota' },
+	{ id: 3, name: 'Berita', path: '/berita' },
+	{ id: 4, name: 'Contact Us', path: '/contact' }
+];
